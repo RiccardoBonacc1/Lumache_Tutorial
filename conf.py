8e235a138bd5735abf6e64ac2231485e7d7e5884
Riccardo Bonacci <121068340+RiccardoBonacc1@users.noreply.github.com>
2025-06-27 11:32:09 +1000

    Update conf.py

--- conf.py
+++ conf.py
@@ -36,8 +36,8 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'furo'
-# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 
 
 html_static_path = ['_static']
